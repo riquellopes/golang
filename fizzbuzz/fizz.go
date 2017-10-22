44c754f10e9204543b5b74a053bf91355cf768b5
Henrique Lopes <riquellopes@gmail.com>
2017-10-22 16:25:00 -0200

    simple refac

--- fizzbuzz/fizz.go
+++ fizzbuzz/fizz.go
@@ -2,13 +2,23 @@ package fizzbuzz
 
 import "strconv"
 
+// Fizz -
+func Fizz(numeric int) bool{
+    return numeric % 3 == 0
+}
+
+// Buzz -
+func Buzz(numeric int) bool {
+    return numeric % 5 == 0
+}
+
 // FizzBuzz - Just improving my golang skill
 func FizzBuzz(numeric int) string {
-    if numeric % 3 == 0 && numeric % 5 == 0 {
+    if Fizz(numeric) && Buzz(numeric) {
         return "fizzbuzz"
-    } else if numeric % 3 == 0 {
+    } else if Fizz(numeric) {
         return "fizz"
-    } else if numeric % 5 == 0 {
+    } else if Buzz(numeric) {
         return "buzz"
     }
     return strconv.Itoa(numeric)
