184695680c9d105f7c92f23a7404c3657d02f9ca
Henrique <everaldo.lopes@concrete.com.br>
2017-10-27 11:11:32 -0200

    implementing test.

--- arithmetic/main.go
+++ arithmetic/main.go
@@ -2,6 +2,17 @@ package main
 
 import "log"
 
+// Arithmetic -
+type Arithmetic struct {
+  x int
+  y int
+}
+
+// ToCalc -
+func (a *Arithmetic) ToCalc () (int, int, int){
+  return (a.x + a.y), (a.x - a.y), (a.x * a.y)
+}
+
 
 func main() {
     log.Println("Shows")
--- arithmetic/main_test.go
+++ arithmetic/main_test.go
@@ -5,10 +5,20 @@ import (
       "github.com/stretchr/testify/assert"
 )
 
-func Test(t *testing.T) {
-  a, b, c := Arithmetic{3, 2}
+func TestCalc_1(t *testing.T) {
+  ari := Arithmetic{3, 2}
+  a, b, c := ari.ToCalc()
 
   assert.Equal(t, a, 5)
   assert.Equal(t, b, 1)
   assert.Equal(t, c, 6)
+}
+
+func TestCalc_2(t *testing.T) {
+  ari := Arithmetic{5, 2}
+  a, b, c := ari.ToCalc()
+
+  assert.Equal(t, a, 7)
+  assert.Equal(t, b, 3)
+  assert.Equal(t, c, 10)
 }
