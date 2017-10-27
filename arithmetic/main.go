042d10fa16e4794528ffb6b227245988ac0f1e49
Henrique <everaldo.lopes@concrete.com.br>
2017-10-27 11:33:29 -0200

    refactored

--- arithmetic/main.go
+++ arithmetic/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "log"
+import (
+    "log"
+    "os"
+    "strconv"
+)
 
 // Arithmetic -
 type Arithmetic struct {
@@ -10,10 +14,34 @@ type Arithmetic struct {
 
 // ToCalc -
 func (a *Arithmetic) ToCalc () (int, int, int){
-  return (a.x + a.y), (a.x - a.y), (a.x * a.y)
+  return a.Sum(), a.Difference(), a.Product()
 }
 
+// Sum -
+func (a *Arithmetic) Sum() int {
+  return (a.x + a.y)
+}
+
+// Difference -
+func (a *Arithmetic) Difference() int {
+  return (a.x - a.y)
+}
+
+// Product -
+func (a *Arithmetic) Product() int {
+  return (a.x * a.y)
+}
 
 func main() {
-    log.Println("Shows")
+    x, err := strconv.Atoi(os.Args[1])
+    y, err := strconv.Atoi(os.Args[2])
+
+    if err != nil {
+      log.Println(err)
+      os.Exit(2)
+    }
+
+    ari := Arithmetic{x, y}
+
+    log.Println(ari.ToCalc())
 }
