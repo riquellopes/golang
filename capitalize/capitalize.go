7da5b0fe7664073ccc091230edeabab1fc4ca89d
Henrique Lopes <riquellopes@gmail.com>
2017-10-30 20:25:07 -0200

    simple tests for the Capitalize function.

--- capitalize/capitalize.go
+++ capitalize/capitalize.go
@@ -1,7 +1,14 @@
 package capitalize
 
+import "strings"
 
 // Capitalize -
 func Capitalize(text string) string {
-    return "Hello World"
+	data := strings.Split(text, " ")
+
+	for pos, value := range data {
+		data[pos] = strings.Title(value)
+	}
+
+	return strings.Join(data, " ")
 }
--- capitalize/capitalize_test.go
+++ capitalize/capitalize_test.go
@@ -1,13 +1,31 @@
 package capitalize
 
 import (
-    "testing"
-    "github.com/stretchr/testify/assert"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
+func TestCap_1(t *testing.T) {
+	cap := Capitalize("hello world")
 
-func TestCap_1(t *testing.T){
-    cap := Capitalize("hello world")
+	assert.Equal(t, cap, "Hello World")
+}
 
-    assert.Equal(t, cap, "Hello World")
+func TestCap_2(t *testing.T) {
+	cap := Capitalize("12abc")
+
+	assert.Equal(t, cap, "12abc")
+}
+
+func TestCap_3(t *testing.T) {
+	cap := Capitalize("1 2 3 4 5 6 8")
+
+	assert.Equal(t, cap, "1 2 3 4 5 6 8")
+}
+
+func TestCap_4(t *testing.T) {
+	cap := Capitalize("q w e r t y u i o p a s d f g h j  k l z x c v b n m Q W E R T Y U I O P A S D F G H J  K L Z X C V B N M")
+
+	assert.Equal(t, cap, "Q W E R T Y U I O P A S D F G H J  K L Z X C V B N M Q W E R T Y U I O P A S D F G H J  K L Z X C V B N M")
 }
