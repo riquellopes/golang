7370b9a798b61025fc6bf23c6759c78905722748
Henrique Lopes <riquellopes@gmail.com>
2017-11-17 22:16:34 -0200

    creating new tests

--- write-a-function/leap_year_test.go
+++ write-a-function/leap_year_test.go
@@ -17,3 +17,23 @@ func Test_2400_is_true(t *testing.T) {
 func Test_1800_is_true(t *testing.T) {
 	assert.False(t, isLeap(1800))
 }
+
+func Test_1900_is_true(t *testing.T) {
+	assert.False(t, isLeap(1900))
+}
+
+func Test_2100_is_true(t *testing.T) {
+	assert.False(t, isLeap(2100))
+}
+
+func Test_2200_is_true(t *testing.T) {
+	assert.False(t, isLeap(2200))
+}
+
+func Test_2300_is_true(t *testing.T) {
+	assert.False(t, isLeap(2300))
+}
+
+func Test_2500_is_true(t *testing.T) {
+	assert.False(t, isLeap(2500))
+}
