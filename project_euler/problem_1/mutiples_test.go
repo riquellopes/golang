5ecd4bb186eea4d4ab22cef7422c934f3d0054fd
Henrique Lopes <riquellopes@gmail.com>
2017-11-25 08:01:57 -0200

    problem ok

--- project_euler/problem_1/mutiples_test.go
+++ project_euler/problem_1/mutiples_test.go
@@ -13,3 +13,7 @@ func TestShouldGet23(t *testing.T) {
 func TestShouldGet14(t *testing.T) {
 	assert.Equal(t, Multiple(9), 14)
 }
+
+func TestShouldGet233168(t *testing.T) {
+	assert.Equal(t, Multiple(1000), 233168)
+}
