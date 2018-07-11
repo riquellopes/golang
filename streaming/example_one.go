df916c95579d7f3aca90d3310e69ef4d87d15a22
Henrique Lopes <riquellopes@gmail.com>
2018-07-10 22:15:27 -0300

    adding the first streaming example

---
+++ streaming/example_one.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+func main() {
+	reader := strings.NewReader("First example with streaming")
+	p := make([]byte, 4)
+
+	for {
+		n, err := reader.Read(p)
+
+		if err == io.EOF {
+			break
+		}
+
+		fmt.Println(string(p[:n]))
+	}
+}
