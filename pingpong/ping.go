0e2a5a3fc8aad61a577b673770dad92599de35b3
Henrique Lopes <riquellopes@gmail.com>
2022-01-23 15:42:43 -0300

    updated declaration

--- pingpong/ping.go
+++ pingpong/ping.go
@@ -6,20 +6,20 @@ import (
 )
 
 func ping(number int) string {
-	var ping, pong, num string = "", "", ""
-
+	var (
+		ping = ""
+		pong = ""
+		num  = ""
+	)
 	if divisible(number, 3) {
 		ping = "Ping"
 	}
-
 	if divisible(number, 5) {
 		pong = "Pong"
 	}
-
 	if len(ping) == 0 && len(pong) == 0 {
 		num = strconv.Itoa(number)
 	}
-
 	return fmt.Sprintf("%s%s%s", ping, pong, num)
 }
 
