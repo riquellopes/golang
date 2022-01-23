2210786669fdc8e3301583be31aced13c808d1c7
Henrique Lopes <riquellopes@gmail.com>
2022-01-23 15:38:15 -0300

    removed unusable func

--- pingpong/ping.go
+++ pingpong/ping.go
@@ -1,26 +1,31 @@
 package pingpong
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
 func ping(number int) string {
-    if doubleDivisor(number){
-        return "PingPong"
-    } else if divisible(number, 3) {
-        return "Ping"
-    } else if divisible(number, 5){
-        return "Pong"
-    }
+	var ping, pong, num string = "", "", ""
 
-    return strconv.Itoa(number)
+	if divisible(number, 3) {
+		ping = "Ping"
+	}
+
+	if divisible(number, 5) {
+		pong = "Pong"
+	}
+
+	if len(ping) == 0 && len(pong) == 0 {
+		num = strconv.Itoa(number)
+	}
+
+	return fmt.Sprintf("%s%s%s", ping, pong, num)
 }
 
 func divisible(number, divider int) bool {
-    if (number % divider) == 0 {
-        return true
-    }
-    return false
-}
-
-func doubleDivisor(number int) bool{
-    return divisible(number, 3) && divisible(number, 5)
+	if (number % divider) == 0 {
+		return true
+	}
+	return false
 }
